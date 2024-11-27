03fa5f674b63aa4dd650d0f5f8d798e1e242170d
DestroAmer2020 <116159391+DestroAmer2020@users.noreply.github.com>
2024-11-27 16:11:00 +0200

    Update TASK3BYVLAD.java

--- Lab 3/TASK3BYVLAD.java
+++ Lab 3/TASK3BYVLAD.java
@@ -8,7 +8,6 @@ public class ArrayReverse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Get array size and elements from the user
         System.out.print("Enter the size of the array: ");
         int size = scanner.nextInt();
         int[] array = new int[size];
@@ -18,31 +17,26 @@ public class ArrayReverse {
             array[i] = scanner.nextInt();
         }
 
-        // Reverse the array
         int[] reversedArray = new int[size];
         for (int i = 0; i < size; i++) {
             reversedArray[i] = array[size - i - 1];
         }
 
-        // Print the reversed array
         System.out.print("Reversed array: ");
         for (int num : reversedArray) {
             System.out.print(num + " ");
         }
         System.out.println();
 
-        // Convert array to ArrayList
         ArrayList<Integer> list = new ArrayList<>();
         for (int num : reversedArray) {
             list.add(num);
         }
 
-        // a) Add an element to the end of the list
         System.out.print("Enter a number to add to the end of the list: ");
         int addElement = scanner.nextInt();
         list.add(addElement);
 
-        // b) Remove an element from the list
         System.out.print("Enter the index of the element to remove: ");
         try {
             int removeIndex = scanner.nextInt();
@@ -51,7 +45,6 @@ public class ArrayReverse {
             System.out.println("Invalid index! Unable to remove element.");
         }
 
-        // c) Replace an element in the list
         System.out.print("Enter the index of the element to replace: ");
         int replaceIndex = scanner.nextInt();
         System.out.print("Enter the new value: ");
@@ -63,18 +56,15 @@ public class ArrayReverse {
             System.out.println("Invalid index! Unable to replace element.");
         }
 
-        // d) Sort the list in ascending order
         Collections.sort(list);
         System.out.println("Sorted list in ascending order: " + list);
 
-        // e) Print the elements of the list
         System.out.println("Final list elements: ");
         for (int element : list) {
             System.out.print(element + " ");
         }
         System.out.println();
 
-        // Create an ArithmeticException situation and catch it
         System.out.print("Enter a divisor to divide the first element of the list by: ");
         try {
             int divisor = scanner.nextInt();
