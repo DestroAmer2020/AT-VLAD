35ebfd0455e66e91037db32f8df1454e59d569e5
DestroAmer2020 <116159391+DestroAmer2020@users.noreply.github.com>
2024-12-17 22:55:34 +0200

    Delete Lab 8 directory

--- Lab 8/testArraySize.java
+++
@@ -1,7 +0,0 @@
-import org.testng.annotations.Parameters;
-
-@Parameters({"arraySize"})
-public void testArraySize(int arraySize) {
-    int[] array = task2.initializeArray(arraySize);
-    Assert.assertEquals(array.length, arraySize, "Array size does not match expected value.");
-}
