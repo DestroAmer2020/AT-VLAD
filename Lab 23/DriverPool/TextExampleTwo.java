a57aaab1f8b69e7ad7eac5416e1247858d60b7d8
DestroAmer2020 <116159391+DestroAmer2020@users.noreply.github.com>
2024-12-21 22:36:27 +0200

    Delete Lab 23 directory

--- Lab 23/DriverPool/TextExampleTwo.java
+++
@@ -1,7 +0,0 @@
-@Test(dataProvider = "browserProvider", parallel = true)
-public void parallelTest(String browser) {
-    WebDriver driver = DriverPool.getDriver(browser);
-    driver.get("https://example.com");
-    System.out.println(browser + " Title: " + driver.getTitle());
-    DriverPool.quitDriver();
-}
